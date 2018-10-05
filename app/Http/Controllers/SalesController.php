fcfa0317c52c25b5eca31df5a041c21ba539ceae
Unknown <Robbertschalk@gmail.com>
2018-10-05 11:40:15 +0200

    Sales made

---
+++ app/Http/Controllers/SalesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\GetData;
+use Illuminate\Http\Request;
+
+class SalesController extends Controller
+{
+    public function sales(){
+
+
+        $data = GetData::all();
+
+        return view('sales')
+            ->with('data', $data);
+
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+route::get('/sales', 'SalesController@sales');
